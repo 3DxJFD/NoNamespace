713cc0e8c6d9f0bab0f31cbf9a395e91560c8971
3DxJFD <109030800+3DxJFD@users.noreply.github.com>
2023-12-05 21:26:54 -0600

    registration file

---
+++ NoNamespace.rb
@@ -0,0 +1,17 @@
+module NoNamespace
+    module IntProcess
+  
+      EXTENSION = SketchupExtension.new(
+        'IntProcess',  # The name of the extension as it will appear in SketchUp
+        'NoNamespace/loader.rb'  # The path to the loader file
+      )
+      EXTENSION.instance_eval {
+        self.description= 'Adds an extension.'
+        self.version=     '0.0.0'
+        self.copyright=   "©2023"
+        self.creator=     'JFD / SU Community'
+      }
+      Sketchup.register_extension(EXTENSION, true) 
+  
+    end # extension submodule
+  end # top level namespace module
